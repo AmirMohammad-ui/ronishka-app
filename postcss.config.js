48ead028873f241f4d53d5cba089560b93f9e3b3
amirmuhammad <amirmuhammad@debian.amir>
2021-01-27 18:08:06 -0500

    fixed some bogs

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
-    purgecss({
-      content: ['./**/*.ejs']
-    }),
+    // purgecss({
+    //   content: ['./**/*.ejs']
+    // }),
     require('autoprefixer'),
     require("cssnano")({
       preset:'default'
