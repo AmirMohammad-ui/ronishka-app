9d2f16ed63f4ebd945dc38e79dad84049788de5c
amirmuhammad <amirmuhammad@debian.amir>
2021-01-27 14:19:55 -0500

    minified css and img

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
-    purgecss({
-      content: ['./views/*.ejs']
-    }),
+    // purgecss({
+    //   content: ['./views/*.ejs']
+    // }),
     require('autoprefixer'),
     require("cssnano")({
       preset:'default'
